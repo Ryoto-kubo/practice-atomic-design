7052ae2ce9cfa7598209793cff9d098e8ff30dc7
TaroHiraishi <tarohiraishi1@gmail.com>
2020-04-01 08:35:32 +0900

    vue.config.jsを編集

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   "lintOnSave": false,
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Practice Atomic Design'
+      })
+    ]
+  }
 }
