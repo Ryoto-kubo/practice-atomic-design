66bdb68d7933dfd655d7c01104fa534e2657a057
TaroHiraishi <tarohiraishi1@gmail.com>
2020-03-31 13:28:11 +0900

    ルートへのリンクで、Today.vueを/listでTodayList.vueをレンダリング

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
             <v-icon>mdi-view-dashboard</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
+            <router-link to="/" tag="v-list-item-title">Home</router-link>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
@@ -19,7 +19,7 @@
             <v-icon>mdi-settings</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
+			<router-link to="/list" tag="v-list-item-title">List</router-link>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -34,45 +34,8 @@
     </v-app-bar>
 
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/bXNzZL"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
+      <v-container fluid>
+	  <router-view></router-view>
       </v-container>
     </v-content>
 
---
+++ src/views/TodayList.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>This is TodayList.vue</h1>
+</template>
