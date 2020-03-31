711db01dd939f497cb46463186def504abd58ff0
TaroHiraishi <tarohiraishi1@gmail.com>
2020-03-31 22:24:45 +0900

    views/TodayList.vueをTimeschedule.vueに変更

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import RegisterAddress from '../views/RegisterAddress.vue'
-import TodayList from '../views/TodayList.vue'
+import TimeSchedule from '../views/TimeSchedule.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +12,9 @@ const routes = [
     component: RegisterAddress
   },
   {
-    path: '/list',
-    name: 'TodayList',
-    component: TodayList
+    path: '/timeschedule',
+    name: 'TimeSchedule',
+    component: TimeSchedule
   }
 ]
 
